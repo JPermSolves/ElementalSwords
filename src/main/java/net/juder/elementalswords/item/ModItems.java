10b2d940ba205ab7b160cdcb83aa2db430c278a0
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2022-08-02 21:33:47 -0700

    Add FireSword SwordItem and Tier

--- src/main/java/net/juder/elementalswords/item/ModItems.java
+++ src/main/java/net/juder/elementalswords/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.juder.elementalswords.item;
 
 import net.juder.elementalswords.ElementalSwords;
+import net.juder.elementalswords.item.custom.FireSword;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -11,7 +12,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ElementalSwords.MOD_ID);
 
-    public static final RegistryObject<Item> FIRE_SWORD = ITEMS.register("fire_sword", () -> new Item(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<Item> FIRE_SWORD = ITEMS.register("fire_sword", () -> new FireSword(ModTiers.FIRE_SWORD, 2, 2.0f, new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_COMBAT))));
 
     public static final RegistryObject<Item> ICE_SWORD = ITEMS.register("ice_sword", () -> new Item(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_COMBAT)));
 
---
+++ src/main/java/net/juder/elementalswords/item/ModTiers.java
@@ -0,0 +1,7 @@
+package net.juder.elementalswords.item;
+
+import net.minecraftforge.common.ForgeTier;
+
+public class ModTiers {
+    public static final ForgeTier FIRE_SWORD = new ForgeTier(2, 1400, 2.0f, 2f, 15, null, null);
+}
